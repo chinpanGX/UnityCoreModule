ea59307fd65674b3014cc0d2801986d5239ba55b
ちんぱん <52692512+chinpanGX@users.noreply.github.com>
2024-12-29 14:36:13 +0900

    トグルコンポーネントの追加

---
+++ CoreModuleUnityProject/Assets/AppService/Scripts/Runtime/CustomComponent/CustomToggleButton.cs
@@ -0,0 +1,47 @@
+#nullable enable
+using System;
+using R3;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AppService.Runtime
+{
+    public class CustomToggleButton : MonoBehaviour
+    {
+        [SerializeField] private CustomButton? customButton;
+        [SerializeField] private Image onImage;
+        [SerializeField] private Image offImage;
+        
+        private readonly Subject<bool> onOffSubject = new();
+        public Observable<bool> Activated => onOffSubject;
+        private bool isActivated;
+
+        public void Setup(bool isActivated)
+        {
+            if (customButton == null)
+            {
+                Debug.LogError("CustomButton が null です。");
+                return;
+            }
+            
+            this.isActivated = isActivated;
+            customButton.OnClicked(() =>
+            {
+                this.isActivated = !this.isActivated;
+                onOffSubject.OnNext(this.isActivated);
+                UpdateView();
+            });
+        }
+
+        public void Publish()
+        {
+            onOffSubject.OnNext(isActivated);
+        }
+
+        private void UpdateView()
+        {
+            onImage.gameObject.SetActive(isActivated);
+            offImage.gameObject.SetActive(!isActivated);
+        }
+    }
+}
